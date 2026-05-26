1a9dcd85c316de04b9fc83aeece4d653a1297baf
YagoSchramm <yrtoiu1515@gmail.com>
2026-05-26 20:14:15 -0300

    basic project structure

---
+++ domain/entity/derr/errors.go
@@ -0,0 +1,17 @@
+package derr
+
+var (
+	BadRequestError     = NewBadRequestError("Bad Request")
+	UnauthorizedError   = NewUnauthorizedError("Unauthorized")
+	NotFoundError       = NewNotFoundError("Not Found")
+	InternalServerError = NewInternalError("Internal Server Error")
+)
+
+var (
+	InvalidCredentialsError = NewClientError("INVALID_CREDENTIALS", "Invalid credentials")
+	EmailRequired           = NewClientError("EMAIL_REQUIRED", "Email required")
+	UserAlreadyExists       = NewClientError("USER_ALREADY_EXISTS", "User already exists")
+	PasswordRequired        = NewClientError("PASSWORD_REQUIRED", "Password required")
+	InvalidCredentials      = NewClientError("INVALID_CREDENTIALS", "Invalid credentials")
+	InvalidEmail            = NewClientError("INVALID_EMAIL", "Invalid email")
+)
