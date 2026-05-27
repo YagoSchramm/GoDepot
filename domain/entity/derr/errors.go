16b925c053470f948a459c7c0773e8b3514ac48a
YagoSchramm <yrtoiu1515@gmail.com>
2026-05-26 22:36:14 -0300

    feat(): Neu authentication repository and usecase

--- domain/entity/derr/errors.go
+++ domain/entity/derr/errors.go
@@ -11,6 +11,7 @@ var (
 	InvalidCredentialsError = NewClientError("INVALID_CREDENTIALS", "Invalid credentials")
 	EmailRequired           = NewClientError("EMAIL_REQUIRED", "Email required")
 	UserAlreadyExists       = NewClientError("USER_ALREADY_EXISTS", "User already exists")
+	WeakPassword            = NewClientError("WEAK_PASSWORD", "Weak password")
 	PasswordRequired        = NewClientError("PASSWORD_REQUIRED", "Password required")
 	InvalidCredentials      = NewClientError("INVALID_CREDENTIALS", "Invalid credentials")
 	InvalidEmail            = NewClientError("INVALID_EMAIL", "Invalid email")
--- infrastructure/router/module/auth_module.go
+++ infrastructure/router/module/auth_module.go
@@ -1,5 +1,39 @@
 package module
 
-func NewAuthModule() {
+import (
+	"github.com/YagoSchramm/GoDepot/domain/usecase"
+	"github.com/YagoSchramm/GoDepot/infrastructure/router"
+	"github.com/gorilla/mux"
+)
 
+func NewAuthModule(authUseCase usecase.AuthUseCase, secret string) router.Module {
+	return &authModule{
+		authUseCase: authUseCase,
+		name:        "Auth",
+		path:        "/auth",
+		secret:      secret,
+	}
+}
+
+type authModule struct {
+	authUseCase usecase.AuthUseCase
+	name        string
+	path        string
+	secret      string
+}
+
+func (a *authModule) Middlewares() []mux.MiddlewareFunc {
+	panic("unimplemented")
+}
+
+func (a *authModule) Name() string {
+	panic("unimplemented")
+}
+
+func (a *authModule) Path() string {
+	panic("unimplemented")
+}
+
+func (a *authModule) Routes() []router.RouteDefinition {
+	panic("unimplemented")
 }
